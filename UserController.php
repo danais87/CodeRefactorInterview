5dae8e36eb5377c2451fa2e2ab8423a1b03eabf7
danais87 <36306534+danais87@users.noreply.github.com>
2021-04-08 12:23:27 -0500

    Create UserController.php

---
+++ UserController.php
@@ -0,0 +1,37 @@
+/** REVIEWS
+  * First the variable $user used in the if is not declared becouse the function only receive a $request  
+  * I use ->imput for request
+  * Supposed that method isAdmin() is declared 
+  *
+  *
+  *
+**/
+
+class UserController
+{
+	public function update(Request $request)
+	{
+    $user = User::find($request->input('user_id');
+
+		if ($user->isAdmin()) {
+
+			if ($request->input('mark_as_active')) {		
+				$user->active = true;
+				$user->save();		
+			} elseif ( $request->input('mark_as_inactiveactive') ) {
+				$user->active = false;
+				$user->save();
+			} elseif ($request->input('first_name') || $request->input('last_name') || $request->input('address') ) {
+				$user->first_name = $request->input('first_name') ? $request->input('first_name') : $user->first_name;
+				$user->last_name = $request->input('last_name') ? $request->input('last_name') : $user->last_name;
+				$user->address = $request->input('address') ? $request->input('address') : $user->address;
+				$user->save();
+			} 
+
+			return redirect()->back();
+
+		} else {
+			abort(400, “you do not have access”);
+		}
+	}
+}
